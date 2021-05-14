e26838fc973839dbf405f846b10e79e456a01b71
liuhuayu1554 <liuhuayu1554@emao.com>
2021-05-14 16:04:27 +0800

    修复若干Bug

--- api-server/src/main/java/com/mochat/mochat/dao/mapper/ContactFieldMapper.java
+++ api-server/src/main/java/com/mochat/mochat/dao/mapper/ContactFieldMapper.java
@@ -1,6 +1,6 @@
 package com.mochat.mochat.dao.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.mochat.mochat.dao.entity.ContactFieldEntity;
 
 public interface ContactFieldMapper extends BaseMapper<ContactFieldEntity> {
--- api-server/src/main/java/com/mochat/mochat/dao/mapper/MigrationsMapper.java
+++ api-server/src/main/java/com/mochat/mochat/dao/mapper/MigrationsMapper.java
@@ -1,6 +1,6 @@
 package com.mochat.mochat.dao.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.mochat.mochat.dao.entity.MigrationsEntity;
 
 public interface MigrationsMapper extends BaseMapper<MigrationsEntity> {
